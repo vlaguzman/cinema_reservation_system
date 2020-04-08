827bebb13879465bddf015667849ed535f3a32bd
Vladimir Guzman <guzman.vla@gmail.com>
2020-04-07 21:04:29 -0500

    delete comment

--- app/controllers/movie_reservations_controller.rb
+++ app/controllers/movie_reservations_controller.rb
@@ -4,7 +4,6 @@ class MovieReservationsController < ApplicationController
   end
 
   def create
-    puts params
     MovieReservation.create(movie: Movie.first, user: User.find_by(international_id: params[:cedula]))
   end
 
